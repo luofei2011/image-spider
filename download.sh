66f2620e3891dd98ee2ae397afc0fc8b62d4884e
luofei2011 <luofeihit2010@gmail.com>
2015-07-10 02:40:44 +0000

    finish download.sh

--- download.sh
+++ download.sh
@@ -2,10 +2,19 @@
 
 root=$(pwd)
 logDir=$root/log
+imageDir=$root/images
 
 imageUrlList=$logDir/images_log
 
 # remove repeat
 sort -u $imageUrlList -o $imageUrlList
 
-wget -t 1 -O
+cd $imageDir
+
+# you can change it  to download in background.
+# for url in $(cat $imageUrlList) --> wget .. &
+wget -t 1 -i $imageUrlList
+
+cd -
+
+exit 0
